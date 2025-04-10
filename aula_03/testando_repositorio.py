91b8e63395b14ec66aafd55b96ca6e73739f7b0a
CdnCarvalho <122188145+CdnCarvalho@users.noreply.github.com>
2025-04-10 13:47:55 -0300

    Altera o arquivo, via github

--- aula_03/testando_repositorio.py
+++ aula_03/testando_repositorio.py
@@ -2,3 +2,6 @@ print('realizando teste no repositório')
 print('segunda alteração')
 print('terceira alteração')
 print('alterando o arquivo')
+
+# Alteração sendo realizada via Web, diretamente no GitHub
+print('Alterando no site GitHub')
